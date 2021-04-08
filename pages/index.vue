27806a627968d39272767baa0c853efbb937fd78
Dimitris Pliakos <dimitrispl96@gmail.com>
2021-04-08 22:35:07 +0300

    fix: Fix conflicts

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <h2 class="pa-16">Welcome</h2>
+  <div class="container">
+    <searchBar />
+    <CarouselList />
+    <CarouselList />
+    <CarouselList />
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </div>
 </template>
 
 <script>
-
+import CarouselList from "@/components/CarouselList";
+import searchBar from "@/components/searchBar";
 
 export default {
   components: {
-   
-  }
-}
+    CarouselList,
+    searchBar,
+  },
+};
 </script>
+
+<style>
+</style>
